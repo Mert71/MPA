743eff9180ba6eff849443137b3fdec5a4eaf40c
Mert71 <99048479@mydavinci.nl>
2021-01-14 02:17:05 +0100

    Added Laravel with authentication and index page showing psuedo products
    Laravel 7
    
    Migrations and seedings created to create the database with psuedo information
    
    Basic styling

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center">
+    <h2>Products</h2>
+
+    <div class="row">
+
+        @foreach ($allProducts as $product)
+
+    <div class="col-3">
+        <div class="card">
+            <img class="card-img-top" src="{{asset ('default.jpg')}}" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">{{$product->name}}</h4>
+                <p class="card-text">{{$product->description}}</p>
+                <p class="card-text">${{$product->price}}</p>
+            </div>
+            <div class="card-body">
+                <a href="#" class="card-link">Add to cart</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+        @endforeach
+
+
+
+    </div>
+</div>
+@endsection
